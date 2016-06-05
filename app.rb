93abfb53ccf9c52980ae4a34d1099147911296fa
Michael Norelli <mnorelli360@gmail.com>
2016-06-05 15:44:52 -0700

    breaks out view partials, adds links in footer and contact and about pages

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
 class Places < Sinatra::Base
 
   get '/' do
+    @nav = :nav_home
+    @top = :jumbotron
+    @posts = :default
+    erb :home
+  end
+
+  get '/about' do
+    @nav = :nav_about
+    @top = :about
+    @posts = :none
+    erb :home
+  end
+
+  get '/contact' do
+    @nav = :nav_contact
+    @top = :contact
+    @posts = :none
     erb :home
   end
 
