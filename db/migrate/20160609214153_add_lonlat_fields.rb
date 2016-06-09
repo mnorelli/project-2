805a97af8fa7ef8078a199ff596b34685a387175
Michael Norelli <mnorelli360@gmail.com>
2016-06-09 14:57:05 -0700

    edits db to include longitude and latitude columns

--- db/migrate/20160609214153_add_lonlat_fields.rb
+++ db/migrate/20160609214153_add_lonlat_fields.rb
@@ -1,4 +1,6 @@
 class AddLonlatFields < ActiveRecord::Migration
   def change
+    add_column :places, :longitude, :decimal
+    add_column :places, :latitude, :decimal
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160609214153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "places", force: :cascade do |t|
+    t.string   "title"
+    t.string   "location"
+    t.string   "description"
+    t.string   "photo_url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.decimal  "longitude"
+    t.decimal  "latitude"
+  end
 
 end
